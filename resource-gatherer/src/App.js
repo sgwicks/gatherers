2e072cc00bf286bbd75b74a139efb5e7c0d81524
sgwicks <samgwicks@gmail.com>
2020-04-03 12:04:12 +0100

    Moved functionality from App to Grid for less drilling

--- resource-gatherer/src/App.js
+++ resource-gatherer/src/App.js
@@ -2,93 +2,18 @@ import React from "react";
 import Header from "./components/Header";
 import "./App.css";
 import Grid from "./components/Grid";
-import Stockpile from "./components/Stockpile";
 
 class App extends React.Component {
-  state = {
-    stockpile: 1,
-    gridLayout: [
-      ["square"],
-      ['square', "apple"],
-      ["square"],
-      ["square"],
-      ["square"],
-      ["square"],
-      ['square', "apple"],
-      ['square', "apple"],
-      ["square"],
-      ["square"],
-      ["square"],
-      ['square', "apple"],
-      ['tent'],
-      ['square', "apple"],
-      ["square"],
-      ["square"],
-      ["square"],
-      ['square', "apple"],
-      ['square', "apple"],
-      ["square"],
-      ["square"],
-      ["square"],
-      ['square', "apple"],
-      ['square', "apple"],
-      ['square', "apple"]
-        ],
-    target: "",
-    targetParent: ""
-  };
+  
 
   render() {
     return (
       <div className="App">
         <Header />
-        <Grid
-          appleClick={this.appleClick}
-          expandTent={this.expandTent}
-          gridLayout={this.state.gridLayout}
-          handleMouse={this.handleMouse}
-        />
-        <Stockpile stockpile={this.state.stockpile} />
+        <Grid />
       </div>
     );
-  }
-
-  handleMouse = event => {
-    this.setState({
-      target: event.target.className,
-      targetParent: event.target.parentNode.className
-    });
-  };
-
-  appleClick = key => {
-    if (this.state.targetParent === "grid-tent") {
-      this.setState(currentState => {
-        return { 
-          stockpile: currentState.stockpile + 1,
-          gridLayout: currentState.gridLayout.map((square, i) => {
-            return i === key
-            ? [square[0]]
-            : square
-          })
-         };
-      });
     }
-  };
-
-  expandTent = key => {
-    if (this.state.stockpile > 0 && this.state.target === 'grid-square') {
-      this.setState(currentState => {
-        return {
-          stockpile: currentState.stockpile - 1,
-          gridLayout: currentState.gridLayout.map((square, i) => {
-            return i === key
-            ? square.map(item => item === 'square' ? 'tent' : item)
-            : square;
-          })
-        };
-      });
-    }
-  };
 }
 
 export default App;
--- resource-gatherer/src/components/Grid.js
+++ resource-gatherer/src/components/Grid.js
@@ -1,25 +1,103 @@
 import React from "react";
 import GridSquare from "./GridSquare";
+import Stockpile from "./Stockpile";
 
-const Grid = ({ appleClick, expandTent, gridLayout, handleMouse }) => {
+class Grid extends React.Component {
+  state = {
+    stockpile: 1,
+    gridLayout: [
+      ["square"],
+      ['square', "apple"],
+      ["square"],
+      ["square"],
+      ["square"],
+      ["square"],
+      ['square', "apple"],
+      ['square', "apple"],
+      ["square"],
+      ["square"],
+      ["square"],
+      ['square', "apple"],
+      ['tent'],
+      ['square', "apple"],
+      ["square"],
+      ["square"],
+      ["square"],
+      ['square', "apple"],
+      ['square', "apple"],
+      ["square"],
+      ["square"],
+      ["square"],
+      ['square', "apple"],
+      ['square', "apple"],
+      ['square', "apple"]
+        ],
+    target: "",
+    targetParent: ""
+  };
+
+  render()
+  {
   return (
+    <>
     <div className="grid">
-      {/* <div className="grid-center"></div> */}
-      {gridLayout.map((square, i) => {
+      {this.state.gridLayout.map((square, i) => {
         return (
           <GridSquare
             i={i}
-            handleMouse={handleMouse}
+            handleMouse={this.handleMouse}
             className={`grid-${square[0]}`}
-            expandTent={expandTent}
+            expandTent={this.expandTent}
             key={i}
             apple={square[1]}
-            appleClick={appleClick}
+            appleClick={this.appleClick}
           />
+          
         );
       })}
     </div>
-  );
+    <Stockpile stockpile={this.state.stockpile}/>
+    </>
+  );}
+
+  handleMouse = event => {
+    this.setState({
+      target: event.target.className,
+      targetParent: event.target.parentNode.className
+    });
+  };
+
+
+
+  appleClick = key => {
+    if (this.state.targetParent === "grid-tent") {
+      this.setState(currentState => {
+        return { 
+          stockpile: currentState.stockpile + 1,
+          gridLayout: currentState.gridLayout.map((square, i) => {
+            return i === key
+            ? [square[0]]
+            : square
+          })
+         };
+      });
+    }
+  };
+
+  expandTent = key => {
+    if (this.state.stockpile > 0 && this.state.target === 'grid-square') {
+      this.setState(currentState => {
+        return {
+          stockpile: currentState.stockpile - 1,
+          gridLayout: currentState.gridLayout.map((square, i) => {
+            return i === key
+            ? square.map(item => item === 'square' ? 'tent' : item)
+            : square;
+          })
+        };
+      });
+    }
+  };
 };
 
 export default Grid;
